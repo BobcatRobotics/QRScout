0bfdd9764290160a41ab52ca6b1fd8af42abbc21
Jaspreet D <d.jaspreet@gmail.com>
2026-02-17 23:20:21 -0500

    updated logo styles

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,74 @@
 
+const defaultStyle = {
+  fill: '#8e191c',
+  fillOpacity: 1,
+};
+const strokeStyle = {
+  stroke: '#000000',
+  strokeWidth: 0,
+};
+
+const strokeStyle1 = {
+  stroke: '#000000',
+  strokeWidth: 0.264493,
+  strokeDasharray: 'none',
+};
+
+
+const strokeStyle2 = {
+  stroke: '#000000',
+  strokeWidth: 1.8718,
+  strokeDasharray: 'none',
+  strokeOpacity: 1,
+  paintOrder: 'markers stroke fill',
+};
+
+const style2 = {
+  ...defaultStyle,
+  ...strokeStyle2,
+  fontStyle: 'normal',
+  fontVariant: 'normal',
+  fontWeight: 'normal',
+  fontStretch: 'normal',
+  fontSize: '52px',
+  fontFamily: 'Apple SD Gothic Neo',
+  fill: '#ffcc02',
+  fillOpacity: 1,
+};
+
+const style3 = {
+  ...defaultStyle,
+  ...strokeStyle2,
+  fontStyle: 'normal',
+  fontVariant: 'normal',
+  fontWeight: 'normal',
+  fontStretch: 'normal',
+  fontSize: '18px',
+  fontFamily: 'Apple SD Gothic Neo',
+  fill: '#ffcc02',
+  fillOpacity: 1,
+};
+
+const style1 = {
+  ...defaultStyle,
+  display: 'inline',
+  fill: '#8e191c',
+  fillOpacity: 1,
+  ...strokeStyle1,
+};
+
+const style5 = {
+  ...defaultStyle,
+  ...strokeStyle,
+  strokeDasharray: 'none',
+};
+const style4 = {
+  ...defaultStyle,
+  fontSize: '21.0016px',
+  fontFamily: 'SF Sports Night NS',
+  ...strokeStyle,
+};
+
 export function Logo() {
   return (
     <svg
@@ -7,8 +77,7 @@ export function Logo() {
    viewBox="0 0 199.845 43.811769"
    version="1.1"
    id="svg1"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:svg="http://www.w3.org/2000/svg">
+   xmlns="http://www.w3.org/2000/svg">
   <defs
      id="defs1">
     <rect
@@ -28,41 +97,38 @@ export function Logo() {
      id="layer1"
      transform="translate(-5.5949847,-55.371955)">
     <path
-       style="display:inline;fill:#8e191c;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:0.264493;stroke-dasharray:none"
+       style={style1}
        d="M 37.436221,24.935599 175.38924,24.804367 163.28987,37.97116 49.511326,37.70835 Z"
        id="path1"
        transform="matrix(1.4413966,0,0,0.60994692,-47.838925,58.144696)" />
     <text
-       xml:space="preserve"
-       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:52.2057px;font-family:'Apple SD Gothic Neo';-inkscape-font-specification:'Apple SD Gothic Neo, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;text-align:start;writing-mode:lr-tb;direction:ltr;text-anchor:start;mix-blend-mode:normal;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1.8718;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
+       style={style2}
        x="19.264305"
        y="97.363144"
        id="text3"
        transform="scale(1.0013694,0.99863247)"><tspan
          id="tspan3"
-         style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:52.2057px;font-family:'Apple SD Gothic Neo';-inkscape-font-specification:'Apple SD Gothic Neo, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1.8718;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
+         style={style2}
          x="19.264305"
          y="97.363144">177</tspan></text>
     <text
-       xml:space="preserve"
-       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:17.8096px;font-family:Arial;-inkscape-font-specification:Arial;text-align:start;writing-mode:lr-tb;direction:ltr;text-anchor:start;mix-blend-mode:normal;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
+       style={style3}
        x="107.90097"
        y="70.221939"
        id="text3-9"
        transform="scale(0.97343742,1.0272874)"><tspan
          id="tspan3-1"
-         style="font-size:17.8096px;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
+         style={style3}
          x="107.90097"
          y="70.221939">BOBCAT</tspan></text>
     <text
-       xml:space="preserve"
-       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:17.8096px;font-family:Arial;-inkscape-font-specification:Arial;text-align:start;writing-mode:lr-tb;direction:ltr;text-anchor:start;mix-blend-mode:normal;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
+       style={style3}
        x="96.423225"
        y="93.159386"
        id="text3-9-1"
        transform="scale(0.97343742,1.0272874)"><tspan
          id="tspan3-1-5"
-         style="font-size:17.8096px;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
+         style={style3}
          x="96.423225"
          y="93.159386">ROBOTICS</tspan></text>
   </g>
