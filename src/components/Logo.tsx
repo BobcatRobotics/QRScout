32b403af802067729097991b402611f82ffb2f98
Jaspreet D <d.jaspreet@gmail.com>
2026-02-17 22:28:11 -0500

    updated logo file

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,70 +1,14 @@
-const defaultStyle = {
-  fill: '#ef3340',
-  fillOpacity: 1,
-};
-
-const strokeStyle = {
-  stroke: '#000000',
-  strokeWidth: 0,
-};
-
-const style2 = {
-  ...defaultStyle,
-  fontSize: '74.7706px',
-  fontFamily: 'SF Sports Night',
-  ...strokeStyle,
-};
-
-const style3 = {
-  ...defaultStyle,
-  ...strokeStyle,
-  strokeDasharray: 'none',
-};
-
-const style4 = {
-  ...defaultStyle,
-  fontSize: '21.0016px',
-  fontFamily: 'SF Sports Night NS',
-  ...strokeStyle,
-};
 
 export function Logo() {
-  return ( 
-  <svg
+  return (
+    <svg
    width="200.845mm"
    height="44.030998mm"
-   viewBox="-1 -18 200.845 44.030998"
+   viewBox="0 0 199.845 43.811769"
    version="1.1"
    id="svg1"
-   inkscape:version="1.4.2 (ebf0e940, 2025-05-08)"
-   sodipodi:docname="BobcatRobotics-logo1.svg"
-   inkscape:export-filename="Bobcat-logo.png"
-   inkscape:export-xdpi="100"
-   inkscape:export-ydpi="100"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
    xmlns="http://www.w3.org/2000/svg"
    xmlns:svg="http://www.w3.org/2000/svg">
-  <sodipodi:namedview
-     id="namedview1"
-     pagecolor="#ffffff"
-     bordercolor="#000000"
-     borderopacity="0.25"
-     inkscape:showpageshadow="2"
-     inkscape:pageopacity="0.0"
-     inkscape:pagecheckerboard="0"
-     inkscape:deskcolor="#d1d1d1"
-     inkscape:document-units="mm"
-     inkscape:zoom="0.8693993"
-     inkscape:cx="414.65412"
-     inkscape:cy="58.661193"
-     inkscape:window-width="1648"
-     inkscape:window-height="701"
-     inkscape:window-x="60"
-     inkscape:window-y="312"
-     inkscape:window-maximized="0"
-     inkscape:current-layer="layer1"
-     inkscape:export-bgcolor="#ffffff00" />
   <defs
      id="defs1">
     <rect
@@ -79,72 +23,48 @@ export function Logo() {
        width="193.15617"
        height="97.883194"
        id="rect1" />
-    <inkscape:path-effect
-       effect="powerstroke"
-       message=""
-       id="path-effect1"
-       is_visible="true"
-       lpeversion="1.3"
-       scale_width="1"
-       interpolator_type="CubicBezierJohan"
-       interpolator_beta="0.2"
-       start_linecap_type="zerowidth"
-       end_linecap_type="zerowidth"
-       offset_points="0.48139333,-0.45526121"
-       linejoin_type="extrp_arc"
-       miter_limit="4"
-       not_jump="false"
-       sort_points="true" />
   </defs>
   <g
-     inkscape:label="Layer 1"
-     inkscape:groupmode="layer"
      id="layer1"
      transform="translate(-5.5949847,-55.371955)">
     <path
        style="display:inline;fill:#8e191c;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:0.264493;stroke-dasharray:none"
        d="M 37.436221,24.935599 175.38924,24.804367 163.28987,37.97116 49.511326,37.70835 Z"
        id="path1"
-       inkscape:path-effect="#path-effect1"
-       inkscape:original-d="M 37.436221,24.935599 175.38924,24.804367 163.28987,37.97116 49.511326,37.70835 Z"
-       sodipodi:nodetypes="ccccc"
-       transform="matrix(1.4413966,0,0,0.60994692,-48.365457,40.242608)" />
+       transform="matrix(1.4413966,0,0,0.60994692,-47.838925,58.144696)" />
     <text
        xml:space="preserve"
        style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:52.2057px;font-family:'Apple SD Gothic Neo';-inkscape-font-specification:'Apple SD Gothic Neo, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;text-align:start;writing-mode:lr-tb;direction:ltr;text-anchor:start;mix-blend-mode:normal;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1.8718;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
-       x="18.738493"
-       y="79.436569"
+       x="19.264305"
+       y="97.363144"
        id="text3"
-       transform="scale(1.0013694,0.99863242)"><tspan
+       transform="scale(1.0013694,0.99863247)"><tspan
          id="tspan3"
          style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:52.2057px;font-family:'Apple SD Gothic Neo';-inkscape-font-specification:'Apple SD Gothic Neo, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1.8718;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
-         x="18.738493"
-         y="79.436569"
-         sodipodi:role="line">177</tspan></text>
+         x="19.264305"
+         y="97.363144">177</tspan></text>
     <text
        xml:space="preserve"
        style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:17.8096px;font-family:Arial;-inkscape-font-specification:Arial;text-align:start;writing-mode:lr-tb;direction:ltr;text-anchor:start;mix-blend-mode:normal;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
-       x="107.36007"
-       y="52.795403"
+       x="107.90097"
+       y="70.221939"
        id="text3-9"
        transform="scale(0.97343742,1.0272874)"><tspan
-         sodipodi:role="line"
          id="tspan3-1"
          style="font-size:17.8096px;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
-         x="107.36007"
-         y="52.795403">BOBCAT</tspan></text>
+         x="107.90097"
+         y="70.221939">BOBCAT</tspan></text>
     <text
        xml:space="preserve"
        style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:17.8096px;font-family:Arial;-inkscape-font-specification:Arial;text-align:start;writing-mode:lr-tb;direction:ltr;text-anchor:start;mix-blend-mode:normal;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
-       x="95.882324"
-       y="75.732933"
+       x="96.423225"
+       y="93.159386"
        id="text3-9-1"
        transform="scale(0.97343742,1.0272874)"><tspan
-         sodipodi:role="line"
          id="tspan3-1-5"
          style="font-size:17.8096px;fill:#ffcc02;fill-opacity:1;stroke:#8e191c;stroke-width:1;stroke-dasharray:none;stroke-opacity:1;paint-order:markers stroke fill"
-         x="95.882324"
-         y="75.732933">ROBOTICS</tspan></text>
+         x="96.423225"
+         y="93.159386">ROBOTICS</tspan></text>
   </g>
 </svg>
   );
